d5d2cb46ecdd7ab17e24250f53ef501cf9adbd30
Vivek Singh <vivek.singh@iphtechnologies.com>
2023-12-22 19:02:16 +0530

    created country list with the mvc pattern using local json

---
+++ lib/controllers/countrylistcontroller.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:country_lists/models/countrylistmodel.dart';
+import 'package:flutter/services.dart';
+
+class CountryListController {
+
+
+  Future<List<CountryModel>> fetchData() async {
+    try {
+      String jsonData = await rootBundle.loadString('assets/api/sample.json');
+      List<dynamic> data = json.decode(jsonData)['items'];
+      return data.map((item) => CountryModel.fromJson(item)).toList(); //convert data list of dynamic to the country model object using factory method by mapping them to list
+    } catch (e) {
+      print("Error: $e");
+      return [];
+    }
+  }
+}
---
+++ lib/models/countrylistmodel.dart
@@ -0,0 +1,22 @@
+class CountryModel {
+
+  final String? image;
+  final String? title;
+  final String? subtitle;
+  // final String? link;
+
+
+  CountryModel({required this.image,this.title,this.subtitle}); //constructor 
+
+  factory CountryModel.fromJson(Map<String,dynamic> json) //factory method to create the instance of the country model
+  {
+    return CountryModel(  //return the object of this class model
+    image: json['image'],
+    title: json['title'],
+    subtitle: json['subtitle'],
+    // link:json['link']
+
+    );
+  }
+}
+
